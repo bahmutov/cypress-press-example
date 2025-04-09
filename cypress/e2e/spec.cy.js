d32533129f541a79789f559a89ac6468f41a4cf7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-04-09 15:35:24 -0400

    feat: add form submission with validation (#2)
    * adding the cypress test
    
    * working test
    
    * feat: add form submission with validation
    
    * submit form

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,25 +1,30 @@
-// enables intelligent code completion for Cypress commands
-// https://on.cypress.io/intelligent-code-completion
 /// <reference types="cypress" />
 
-describe('Example Cypress TodoMVC test', () => {
+describe('Login Form', () => {
   beforeEach(() => {
-    // usually we recommend setting baseUrl in cypress.json
-    // but for simplicity of this example we just use it here
-    // https://on.cypress.io/visit
-    cy.visit('http://todomvc.com/examples/vue/')
+    cy.visit('index.html')
   })
 
-  it('adds 2 todos', function () {
-    cy.get('.new-todo')
-      .type('learn testing{enter}')
-      .type('be cool{enter}')
-    cy.get('.todo-list li').should('have.length', 2)
-  })
+  it('should use tab to navigate through the form', () => {
+    cy.get('#username').type('joe').press(Cypress.Keyboard.Keys.TAB)
+    cy.get('#email')
+      .should('be.focused')
+      .type('joe@example.com')
+      .press(Cypress.Keyboard.Keys.TAB)
+    cy.get('#password')
+      .should('be.focused')
+      .type('pa$$w0rd')
+      .press(Cypress.Keyboard.Keys.TAB)
+    cy.get('#terms')
+      .should('be.focused')
+      .check()
+      .press(Cypress.Keyboard.Keys.TAB)
 
-  // more examples
-  //
-  // https://github.com/cypress-io/cypress-example-todomvc
-  // https://github.com/cypress-io/cypress-example-kitchensink
-  // https://on.cypress.io/writing-your-first-test
+    cy.get('.error-message').should('not.be.visible')
+    cy.intercept('POST', '/login', { body: { success: true } }).as(
+      'login',
+    )
+    cy.get('.submit-button').should('be.focused').click()
+    cy.wait('@login')
+  })
 })
