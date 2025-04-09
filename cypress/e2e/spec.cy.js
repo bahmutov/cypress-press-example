4d01fe03ff1b98f90f393970629a0f9d7562bda5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-04-09 15:26:44 -0400

    working test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -6,6 +6,21 @@ describe('Login Form', () => {
   })
 
   it('should use tab to navigate through the form', () => {
-    cy.focused().should('match', 'input#username')
+    cy.get('#username').type('joe').press(Cypress.Keyboard.Keys.TAB)
+    cy.get('#email')
+      .should('be.focused')
+      .type('joe@example.com')
+      .press(Cypress.Keyboard.Keys.TAB)
+    cy.get('#password')
+      .should('be.focused')
+      .type('pa$$w0rd')
+      .press(Cypress.Keyboard.Keys.TAB)
+    cy.get('#terms')
+      .should('be.focused')
+      .check()
+      .press(Cypress.Keyboard.Keys.TAB)
+    cy.get('.submit-button').should('be.focused').click()
+
+    cy.get('.error-message').should('not.be.visible')
   })
 })
